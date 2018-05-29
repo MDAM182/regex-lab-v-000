422c7a5cbfe90193f3c0c3b647615cc05e15bb9a
Michael Morris <mdam2345@gmail.com>
2018-05-29 23:56:11 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,10 +11,11 @@ text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-!!text.match(/^[A-Z].+[\.!?]$/) 
+!!text.match(/^[A-Z].+[\.!?]$/)
 
 end
 
 def valid_phone_number?(phone)
+  +phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
 
 end
